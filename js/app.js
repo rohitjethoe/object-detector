aa6c962b35bf20b0912cfa0108384120c7b6f686
rohitjethoe <rohitchai@icloud.com>
2020-09-19 10:14:36 +0200

    Timecounter for each image.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
+const onLoadTime = new Date().getTime();
+
 mobilenet.load().then(model => {
     for(let i = 0; i < 3; i++) {
+        let time = new Date().getTime();
+
         const img = document.querySelector(`.image-${i}`);
 
+        time = (time - onLoadTime) / 2000;
+
         model.classify(img).then(predictions => {
-            console.log('Predictions: ');
-            console.log(predictions);
-            document.querySelector(`.type-${i}`).innerHTML = predictions[0].className;
+            document.querySelector(`.type-${i}`).innerHTML = `${predictions[0].className} in ${time} seconds`;
         });
     }
 });
