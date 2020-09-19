900c477ba6e43dba51b596f35be8358c6f892ece
rohitjethoe <rohitchai@icloud.com>
2020-09-19 09:55:12 +0200

    Created a dog detection.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 
 mobilenet.load().then(model => {
-    for(let i = 0; i < 1; i++) {
+    for(let i = 0; i < 2; i++) {
         const img = document.querySelector(`.image-${i}`);
 
         model.classify(img).then(predictions => {
             console.log('Predictions: ');
-            console.log(predictions[0]);
+            console.log(predictions);
             document.querySelector(`.type-${i}`).innerHTML = predictions[0].className;
         });
     }
